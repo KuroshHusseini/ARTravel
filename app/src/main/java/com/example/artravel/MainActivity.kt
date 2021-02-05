fbc9ea59079c5c4fa0ac777d1a56b92ac64c9098
kurosh97 <50096460+kurosh97@users.noreply.github.com>
2021-02-05 15:18:48 +0200

    bottom navigation done

--- app/src/main/java/com/example/artravel/MainActivity.kt
+++ app/src/main/java/com/example/artravel/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.artravel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViews()
+    }
+
+    fun setupViews() {
+        // Finding the Navigation Controller
+        val navController = findNavController(R.id.fragNavHost)
+
+       val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.mapsFragment,
+                R.id.attractionsFragment,
+                R.id.weatherFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Setting Navigation Controller with the BottomNavigationView
+        bottomNavView.setupWithNavController(navController)
     }
 }
