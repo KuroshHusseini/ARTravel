30bfb2376b74e68be1be0ce95846a69be90ba22b
Michael Lock <thelockymichael@gmail.com>
2021-02-16 11:52:05 +0200

    trying out arcore features

--- app/src/main/java/com/example/artravel/ArTakeImage.kt
+++ app/src/main/java/com/example/artravel/ArTakeImage.kt
@@ -1,22 +1,67 @@
 package com.example.artravel
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.Config
+import com.google.ar.core.Session
+import com.google.ar.core.exceptions.CameraNotAvailableException
+import com.google.ar.core.exceptions.UnavailableApkTooOldException
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
+import uk.co.appoly.arcorelocation.LocationMarker
+import uk.co.appoly.arcorelocation.LocationScene
+import uk.co.appoly.arcorelocation.rendering.AnnotationRenderer
+import uk.co.appoly.arcorelocation.utils.ARLocationPermissionHelper
+
 
 class ArTakeImage : AppCompatActivity() {
     private lateinit var arFrag: ArFragment
     private var viewRenderable: ViewRenderable? = null
 
 
+    // Step 1. ARCore Location 3rd party
+    private lateinit var locationScene: LocationScene
+    private var mSession: Session? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ar_take_image)
+
+        var exception: Exception? = null
+        var message: String? = null
+        try {
+            mSession = Session( /* context= */this)
+        } catch (e: UnavailableArcoreNotInstalledException) {
+            message = "Please install ARCore"
+            exception = e
+        } catch (e: UnavailableApkTooOldException) {
+            message = "Please update ARCore"
+            exception = e
+        } catch (e: UnavailableSdkTooOldException) {
+            message = "Please update this app"
+            exception = e
+        } catch (e: Exception) {
+            message = "This device does not support AR"
+            exception = e
+        }
+
+        if (message != null) {
+            Log.e("PERKELE!", "Excpetion creating session", exception)
+//            showSnackbarMessage(message, true)
+//            Log.e(
+//                com.google.ar.core.examples.java.helloar.HelloArActivity.TAG,
+//                "Exception creating session",
+//                exception
+//            )
+            return
+        }
+
 
         arFrag = supportFragmentManager.findFragmentById(
             R.id.sceneform_fragment
@@ -26,6 +71,27 @@ class ArTakeImage : AppCompatActivity() {
             .build()
         renderableFuture.thenAccept { viewRenderable = it }
 
+        locationScene = LocationScene(this, this, mSession)
+
+        var config = Config(mSession)
+        if (!mSession?.isSupported(config)!!)
+            Log.e("PERKELE!", "This devicde does not support AR", exception)
+
+        mSession!!.configure(config)
+
+
+
+
+
+
+        // Annotation at Buckingham Palace
+        // Shows toast on touch
+
+        var buckinghamPalace: LocationMarker = LocationMarker(
+            0.1419,
+            51.5014,
+            AnnotationRenderer("Buckingham Palace")
+        )
 
         // Set on tap Ar Plane Listener
 
@@ -47,6 +113,29 @@ class ArTakeImage : AppCompatActivity() {
             viewNode.renderable = viewRenderable
             // Sets this as the selected node in the TransformationSystem
             viewNode.select()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // ARCore requires camera permissions to operate. If we did not yet obtain runtime
+        // permission on Android M and above, now is a good time to ask the user for it.
+        if (ARLocationPermissionHelper.hasPermission(this)) {
+            if (locationScene != null) locationScene.resume()
+            if (mSession != null) {
+                showLoadingMessage()
+                // Note that order matters - see the note in onPause(), the reverse applies here.
+                try {
+                    mSession!!.resume()
+                } catch (e: CameraNotAvailableException) {
+                    e.printStackTrace()
+                }
+            }
+            mSurfaceView.onResume()
+            mDisplayRotationHelper.onResume()
+        } else {
+            ARLocationPermissionHelper.requestPermission(this)
         }
     }
 }
