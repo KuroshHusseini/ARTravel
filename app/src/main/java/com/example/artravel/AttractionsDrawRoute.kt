dc5be4d43d1a874e3b5b27d05f131fe3d3225dc4
kurosh97 <50096460+kurosh97@users.noreply.github.com>
2021-02-19 18:33:43 +0200

    ADR code cleaned

--- app/src/main/java/com/example/artravel/AttractionsDrawRoute.kt
+++ app/src/main/java/com/example/artravel/AttractionsDrawRoute.kt
@@ -25,32 +25,25 @@ import com.google.android.gms.maps.model.*
 import com.google.android.material.snackbar.Snackbar
 import java.util.*
 
+@Suppress("DEPRECATION", "NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class AttractionsDrawRoute : Fragment(), RoutingListener {
-
-    val GOOGLE_API_KEY = Constants.GOOGLE_API_KEY
+    companion object {
+        val GOOGLE_API_KEY = Constants.GOOGLE_API_KEY
+        private const val LOCATION_PERMISSION_REQUEST = 1
+    }
 
     private lateinit var map: GoogleMap
-
-    private val LOCATION_PERMISSION_REQUEST = 1
-
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: LocationCallback
-
     // Destination LatLng
     private var destinationLat: String? = null
     private var destinationLng: String? = null
-
     // Destination
-    //             val dataResponses = mutableListOf<PlaceInfoResponse>()
     private val polylines = mutableListOf<Polyline>()
-//    private val polylines: musta<Polyline>? = null
-
     private var userLocation: Location? = null
-
     private var start: LatLng? = null
     private var end: LatLng? = null
-
     private var parentLayout: View? = null
 
     override fun onCreateView(
@@ -60,21 +53,9 @@ class AttractionsDrawRoute : Fragment(), RoutingListener {
         // Inflate the layout for this fragment
         destinationLat = arguments?.getString("lat")
         destinationLng = arguments?.getString("lon")
-
-
         destinationLat?.let { Log.d("WTF", it) }
-
         destinationLng?.let { Log.d("WTF", it) }
-
-//        if (lat != null) {
-//            Log.d("DrawRoute", lat)
-//        }
-//        if (lon != null) {
-//            Log.d("DrawRoute", lon)
-//        }
-
         parentLayout = view?.findViewById(android.R.id.content)
-
         return inflater.inflate(R.layout.fragment_attractions_draw_route, container, false)
     }
 
@@ -83,31 +64,12 @@ class AttractionsDrawRoute : Fragment(), RoutingListener {
         val mapFragment =
             childFragmentManager.findFragmentById(R.id.google_map_draw_route) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
-
         // For locating and updating user location
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
     }
-
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         map = googleMap
         getLocationAccess()
-
         // Use a custom info window adapter to handle multiple lines of text in the
         // info window contents.
         this.map.setInfoWindowAdapter(object : GoogleMap.InfoWindowAdapter {
@@ -161,7 +123,6 @@ class AttractionsDrawRoute : Fragment(), RoutingListener {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-
         if (requestCode == LOCATION_PERMISSION_REQUEST) {
             if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
                 map.isMyLocationEnabled = true
@@ -184,29 +145,13 @@ class AttractionsDrawRoute : Fragment(), RoutingListener {
         locationCallback = object : LocationCallback() {
 
             override fun onLocationResult(locationResult: LocationResult) {
-                // Geocode user location address
-//                val addresses: List<Address>
-
                 if (locationResult.locations.isNotEmpty()) {
                     userLocation = locationResult.lastLocation
 
                     if (userLocation != null) {
-
-
                         activity ?: return
-//                val geocoder = Geocoder(activity!!.application, Locale.getDefault())
-
                         val latLng = LatLng(userLocation!!.latitude, userLocation!!.longitude)
-
-//                        addresses =
-//                            geocoder.getFromLocation(location.latitude, location.longitude, 1)
-
-//                        val address: String = addresses[0].getAddressLine(0)
-
-//                        Log.d("DBG", address)
-
                         map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
-
                         setDestination()
                     }
                 }
@@ -215,13 +160,9 @@ class AttractionsDrawRoute : Fragment(), RoutingListener {
     }
 
     private fun setDestination() {
-
         userLocation ?: return
-
         start = LatLng(userLocation!!.latitude, userLocation!!.longitude)
-
         end = LatLng(destinationLat!!.toDouble(), destinationLng!!.toDouble())
-
         findRoute(start, end)
     }
 
@@ -229,8 +170,7 @@ class AttractionsDrawRoute : Fragment(), RoutingListener {
         if (start == null || end == null) {
             Toast.makeText(activity, "Unable to get location", Toast.LENGTH_LONG).show()
         } else {
-
-            var routing: Routing? = Routing.Builder()
+            val routing: Routing? = Routing.Builder()
                 .travelMode(AbstractRouting.TravelMode.DRIVING)
                 .withListener(this)
                 .alternativeRoutes(true)
@@ -241,8 +181,6 @@ class AttractionsDrawRoute : Fragment(), RoutingListener {
 
         }
     }
-
-
     @SuppressLint("MissingPermission")
     private fun startLocationUpdates() {
         fusedLocationClient.requestLocationUpdates(
@@ -253,63 +191,48 @@ class AttractionsDrawRoute : Fragment(), RoutingListener {
     }
 
     override fun onRoutingFailure(e: RouteException?) {
-//        val parentLayout: View = view?.rootView?.findViewById(android.R.id.content) ?:
-
-//        val parentLayout: View = fragmentManager?.findView(android.R.id.content)
-
         if (parentLayout != null) {
-            val snackbar: Snackbar = Snackbar.make(parentLayout!!, e.toString(), Snackbar.LENGTH_LONG)
+            val snackbar: Snackbar =
+                Snackbar.make(parentLayout!!, e.toString(), Snackbar.LENGTH_LONG)
             snackbar.show()
-
         } else {
-            Toast.makeText(activity, "Routing failed.", Toast.LENGTH_LONG).show();
-
+            Toast.makeText(activity, getString(R.string.routing_failed), Toast.LENGTH_LONG).show()
         }
-
     }
 
     override fun onRoutingStart() {
-        Toast.makeText(activity, "Finding Route...", Toast.LENGTH_LONG).show();
+        Toast.makeText(activity, getString(R.string.finding_routing), Toast.LENGTH_LONG).show()
     }
 
     override fun onRoutingSuccess(route: ArrayList<Route>?, shortestRouteIndex: Int) {
-//        var center: CameraUpdate = CameraUpdateFactory.newLatLngZoom(start)
-//        var zoom: CameraUpdate = CameraUpdateFactory.zoomTo(16f)
-
         polylines.clear()
-
-        var polyOptions = PolylineOptions()
+        val polyOptions = PolylineOptions()
         var polyLineStartLatLng: LatLng? = null
-        var polylineEndLatLng:LatLng? = null
-
+        var polylineEndLatLng: LatLng? = null
         for (i in 0 until route!!.size) {
 
             if (i == shortestRouteIndex) {
                 polyOptions.color(resources.getColor(R.color.colorPrimary))
                 polyOptions.width(7f)
                 polyOptions.addAll(route[shortestRouteIndex].points)
-                var polyline = map.addPolyline(polyOptions)
+
+                val polyline = map.addPolyline(polyOptions)
+
                 polyLineStartLatLng = polyline.points[0]
+                val k: Int = polyline.points.size
 
-                var k: Int = polyline.points.size
-
-                polylineEndLatLng = polyline.points[k-1]
-
+                polylineEndLatLng = polyline.points[k - 1]
                 polylines.add(polyline)
             }
-
-
             // Add Marker on route starting position
-            var startMarker: MarkerOptions? = MarkerOptions()
-
+            val startMarker = MarkerOptions()
             if (polyLineStartLatLng != null) {
-                startMarker?.position(polyLineStartLatLng)
+                startMarker.position(polyLineStartLatLng)
             }
-            startMarker?.title("My Location")
+            startMarker.title("My Location")
             map.addMarker(startMarker)
-
             // Add Marker on route ending position
-            var endMarker = MarkerOptions()
+            val endMarker = MarkerOptions()
             if (polylineEndLatLng != null) {
                 endMarker.position(polylineEndLatLng)
             }
@@ -319,6 +242,6 @@ class AttractionsDrawRoute : Fragment(), RoutingListener {
     }
 
     override fun onRoutingCancelled() {
-        findRoute(start, end);
+        findRoute(start, end)
     }
 }
